9e3a55da86c44baa20370dd61f771efd9aa50378
jbkang <kjb9301@naver.com>
2019-03-29 14:30:41 +0900

    for eject

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,9 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import ShoppingList from 'components/main/ShoppingList';
 import ShoppingDetail from 'components/main/ShoppingDetail';
+import shoppingData from '/public/dummy/goods.js';
+import axios from 'axios';
 
 class HomeContainer extends Component {
+  componentWillMount(){
+    const test = axios.get('/public/dummy/goods.js');
+    console.log(test)
+  }
+
   render() {
+    console.log("런데후");
     return (
       <Fragment>
         <ShoppingList/>
